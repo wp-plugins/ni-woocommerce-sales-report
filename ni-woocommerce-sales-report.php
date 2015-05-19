4c07aa8fb05ef832f1a17181359b30d4dd23af93
anzia <anzia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-19 18:17:53 +0000

    Added Version number
    git-svn-id: https://plugins.svn.wordpress.org/ni-woocommerce-sales-report/trunk@1163551 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ni-woocommerce-sales-report.php
+++ ni-woocommerce-sales-report.php
@@ -3,7 +3,7 @@
 Plugin Name: Ni Woocommerce Sales Report
 Description: Ni Woocommerce Sales Report  help top keep the track of daily, weekly, monthly and yearly sales report. It provides the list of order details.
 Author: Anzar Ahmed
-Version: 1.1
+Version: 1.2
 Author URI: http://mywebdesire.com/
 License: GPLv2
 */
